6030155872cd5d9848a3b0fdff01155a65dd75f3
dobrosav13 <93608451+dobrosav13@users.noreply.github.com>
2022-06-17 20:20:55 +0200

    test

---
+++ SportskiObjekti/src/services/SportsObjectService.java
@@ -0,0 +1,57 @@
+package services;
+
+import java.util.Collection;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.ServletContext;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+
+import beans.SportsObject;
+import data.SportsObjectDAO;
+
+@Path("/products")
+public class SportsObjectService {
+	
+	@Context
+	ServletContext ctx;
+	
+	public SportsObjectService() {
+	}
+	
+	@PostConstruct
+	// ctx polje je null u konstruktoru, mora se pozvati nakon konstruktora (@PostConstruct anotacija)
+	public void init() {
+		// Ovaj objekat se instancira vi�e puta u toku rada aplikacije
+		// Inicijalizacija treba da se obavi samo jednom
+		if (ctx.getAttribute("sportsObjectDAO") == null) {
+	    	String contextPath = ctx.getRealPath("");
+			ctx.setAttribute("sportsObjectDAO", new SportsObjectDAO());
+		}
+	}
+	
+	@GET
+	@Path("/")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Collection<SportsObject> getProducts() {
+		SportsObjectDAO dao = (SportsObjectDAO) ctx.getAttribute("sportsObjectDAO");
+		System.out.println("AAAA");
+		return dao.getSportsObjectsCollection();
+	}
+	
+	@POST
+	@Path("/")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public SportsObject newProduct(SportsObject object) {
+		SportsObjectDAO dao = (SportsObjectDAO) ctx.getAttribute("productDAO");
+		//return dao.save(product);
+		return object;
+	}
+	
+}
